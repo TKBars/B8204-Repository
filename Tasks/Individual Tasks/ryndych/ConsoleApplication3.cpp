e5c826a1df924541951445a05c4cf643d6ddcbaf
RyndychRD <r.roman1234@yandex.ru>
2016-12-19 12:07:03 +1000

    Update ConsoleApplication3.cpp

--- Tasks/Individual Tasks/ryndych/ConsoleApplication3.cpp
+++ Tasks/Individual Tasks/ryndych/ConsoleApplication3.cpp
@@ -5,26 +5,26 @@ using namespace std;
 int main()
 {
 	dekInfo dequeInfo;
-	dequeInfo.head=NULL;
-	dequeInfo.tail=NULL;	
-	dequeInfo.size=0;
+	dequeInfo.head = NULL;
+	dequeInfo.tail = NULL;
+	dequeInfo.size = 0;
 	int caseD;
 	int insVal;
 	bool close = true;
 	while (close) {
-		cout << "choose operation \n 1)insert el. left \n 2)insert el. right \n 3)delete el. left \n 4)delete el. right \n 5)output deque \n 6)close \n";
+		cout << "choose operation \n 1)insert el. left \n 2)insert el. right \n 3)delete el. left \n 4)delete el. right \n 5)output deque size \n 6)close \n";
 		cin >> caseD;
 		switch (caseD)
 		{
-		case 1: cin>>insVal;
-				lPush(dequeInfo,insVal);
+		case 1: cin >> insVal;
+			lPush(dequeInfo, insVal);
 			break;
-		case 2: cin>>insVal; 
-			RPush(dequeInfo,insVal);
+		case 2: cin >> insVal;
+			RPush(dequeInfo, insVal);
 			break;
-		case 3:lPop(dequeInfo);
+		case 3:cout << lPop(dequeInfo)<<"/n";
 			break;
-		case 4:RPop(dequeInfo);
+		case 4:cout << RPop(dequeInfo) << "/n";
 			break;
 		case 5: printDekSize(dequeInfo);
 			break;
@@ -32,6 +32,6 @@ int main()
 			break;
 		}
 	}
-	
-    return 0;
+
+	return 0;
 }
